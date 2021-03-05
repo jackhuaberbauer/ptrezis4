2c8accceabfcc33e528b4c933959c57e54726e6b
Sebastian Duschinger <sebastian.duschinger@portalsystems.de>
2021-03-05 12:59:54 +0100

    Initial commit

---
+++ client/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Simple Express React Webpack Babel Setup Environment  7';
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
