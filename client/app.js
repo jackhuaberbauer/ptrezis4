f1018ea76fc079a37bb1a644a2dd73cefc180551
Sebastian Duschinger <sebastian.duschinger@portalsystems.de>
2021-03-05 13:08:27 +0100

    Add first rezi stuff

--- client/app.js
+++ client/app.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment'
 
-const title = 'My Simple Express React Webpack Babel Setup Environment  7';
-ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
+function ReziContainer() {
+  const [data, setData] = useState(undefined);
+
+  // Get last friday
+  const today = moment();
+  const lastFriday = today.day() == 5 ? today : today.day(-2);
+  console.log("Last friday", lastFriday);
+
+  if (!data) {
+    fetch(`/rezis?date=${lastFriday.format("DD.MM.yyyy")}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+    return React.createElement("div", undefined, "Loading");
+  }
+
+  return React.createElement("div", undefined, "Data loaded");
+}
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(React.createElement(ReziContainer), domContainer);
+
+
--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-const express = require("express")
-let  app = express()
-app.use(express.static('public'))
-app.listen(3000,  () => console.log("Example app listening on port 3000!")); 
+var express = require("express");
+var pt = require("./lib/plattentests");
+var app = express();
+const port = process.env.port || 1337;
+app.use(express.static(__dirname + "/public"));
+// Allow front-end access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+
+app.get("/rezis", async (req, res) => {
+  if (!req.query.date) {res.json("no date given"); return;}
+  const date = req.query.date
+
+  console.log(`Query rezis (date: ${date})`)
+  Number.parseInt()
+  var reziIds = await pt.getRezisForDate(date);
+  const rezis = [];
+  for (const reziId of reziIds) {
+    rezis.push(await pt.getReziDetails(reziId))
+  }
+  res.json(rezis)
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
